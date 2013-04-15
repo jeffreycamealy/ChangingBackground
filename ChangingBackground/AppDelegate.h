a675b8892c52a1f93ba0752bd1eeeedceff56b39
Jeffrey Camealy <jeffrey@orainteractive.com>
2013-04-15 11:34:06 -0500

    Initial Commit

---
+++ ChangingBackground/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ChangingBackground
+//
+//  Created by Jeffrey Camealy on 4/15/13.
+//  Copyright (c) 2013 Ora Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChangingBackground/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChangingBackground
+//
+//  Created by Jeffrey Camealy on 4/15/13.
+//  Copyright (c) 2013 Ora Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
