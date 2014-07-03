56b8ba8314c316214b6a254bd08b349ae12e6a25
Paul Kolesar <paul.kolesar@yahoo.com>
2014-07-03 14:40:18 -0500

    My implementation of the Changing Background solution

--- ChangingBackground/FirstViewController.h
+++ ChangingBackground/FirstViewController.h
@@ -5,8 +5,11 @@
 //  Created by Jeffrey Camealy on 4/15/13.
 //  Copyright (c) 2013 Ora Interactive. All rights reserved.
 //
+//  Paul Kolesar Solution 7/3/14
+//
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface FirstViewController : UIViewController
 
--- ChangingBackground/SecondViewController.h
+++ ChangingBackground/SecondViewController.h
@@ -5,11 +5,13 @@
 //  Created by Jeffrey Camealy on 4/15/13.
 //  Copyright (c) 2013 Ora Interactive. All rights reserved.
 //
+//  Paul Kolesar Solution 7/3/14
+//
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface SecondViewController : UIViewController
 
 - (IBAction)goBackButtonPressed;
-
 @end
